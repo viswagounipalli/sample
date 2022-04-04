c5677bd01944de4df0bcd9b3d2e56090517e1ac8
viswagounipalli <viswagounipalli@gmail.com>
2022-04-04 16:35:49 +0530

    views file has changed

--- myapp/views.py
+++ myapp/views.py
@@ -5,16 +5,7 @@ from django.http import HttpResponse
 import random
 def Home(request):
 	return render(request,"home.html")
-
 def Register_fun(request):
-	if request.method=="POST":
-		register_f=register_form(request.POST)
-		if register_f.is_valid():
-			register_f.save()			
-			request.session["uname"]=register_f.cleaned_data["firstname"]
-			f_data=register.objects.filter(firstname=register_f.cleaned_data["firstname"],email=register_f.cleaned_data["email"])
-			return render(request,"user_inforamation_page.html",{"data":f_data})
-	else:
 		register_f=register_form()
 		return render(request,"register_page.html",{"data":register_f})
 
@@ -24,10 +15,7 @@ def login_fun(request):
 		if login_data:			
 			request.session["uname"]=request.POST["username"]
 			return redirect("userdashboard")
-		#return render(request,"user_inforamation_page.html",{"data":data})
 		else:
-			#login_f=login_form()
-#			wrong_credentails.html
 			return render(request,"wrong_credentails.html",{"data":" "})
 	else:
 		login_f=login_form()
@@ -39,14 +27,16 @@ def update_fun(request,id):
 	if form.is_valid():
 		form.save(commit=True)
 		request.session["uname"]=form.cleaned_data["firstname"]
-	#	f_data=register.objects.filter(firstname=form.cleaned_data["firstname"],email=form.cleaned_data["email"])
 		return redirect("userdashboard")
 	else:
 		return render(request,"update_page.html",{"data":form})
-	
+
+
 def user_dashboard(request):
 	if request.session.has_key("uname"):
 		data_list=register.objects.filter(firstname=request.session["uname"])
+		#data={}
+		#[data.setdefault(k,v) for k,v in data_list.values()[0].items() if k not in("password","id")]
 		return render(request,"user_inforamation_page.html",{"data":data_list})
 	else:
 		return redirect("home")
